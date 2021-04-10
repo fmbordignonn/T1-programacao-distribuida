4e89db520d360e9bdac22a0f27e4653e5a37f06d
Felipe Bordignon <felipe.bordignon@edu.pucrs.br>
2021-04-10 15:16:59 -0300

    conectando e registrando no rmi, interfaces criadas (nao foi testado)

--- JogadorClientImpl.java
+++ JogadorClientImpl.java
@@ -1,6 +1,70 @@
+import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
 
-public class JogadorClientImpl implements JogadorClient {
+public class JogadorClientImpl extends UnicastRemoteObject implements JogadorClient {
+
+    protected JogadorClientImpl() throws RemoteException {
+        super();
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    private static final int port = 52369;
+    private static final String rmiServer = ":" + port + "/JogoServer";
+    private static final String rmiClient = ":" + port + "/JogadorClient";
+
+    public static void main(String[] args) {
+
+        //ADICIONAR NUMBER OF PLAYS
+		if (args.length != 3) {
+			System.out.println("Usage: java AdditionClient <server ip> <client ip> <num of plays>");
+			System.exit(1);
+		}
+	
+		try {
+			System.setProperty("java.rmi.server.hostname", args[1]);
+			LocateRegistry.createRegistry(port);
+			System.out.println("java RMI registry created.");
+		} catch (RemoteException e) {
+			System.out.println("java RMI registry already exists.");
+		}
+
+		try {
+			String client = "rmi://" + args[1] + rmiClient;
+			Naming.rebind(client, new JogadorClientImpl());
+			System.out.println("JogadorClient is ready.");
+		} catch (Exception e) {
+			System.out.println("JogadorClient failed: " + e);
+		}
+
+		String remoteHostName = args[0];
+		String connectLocation = "rmi://" + remoteHostName + rmiServer;
+
+		JogoServer jogoServer = null;
+		try {
+			System.out.println("Connecting to server at : " + connectLocation);
+			jogoServer = (JogoServer) Naming.lookup(connectLocation);
+			System.out.println ("JogoServer is ready.");
+		} catch (Exception e) {
+			System.out.println ("JogoServer failed: ");
+			e.printStackTrace();
+		}
+
+		while (true) {
+			try {
+				jogoServer.registra();
+				System.out.println("Calling server to register" );
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException ex) {}
+		}
+	}
+    
 
     @Override
     public void inicia() throws RemoteException {
--- JogoServerImpl.java
+++ JogoServerImpl.java
@@ -1,6 +1,39 @@
+import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.registry.LocateRegistry;
 
-public class JogoServerImpl implements JogoServer {
+public class JogoServerImpl extends UnicastRemoteObject implements JogoServer {
+
+    protected JogoServerImpl() throws RemoteException {
+        super();
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    public static void main(String[] args) throws RemoteException {
+		if (args.length != 2) {
+            //ADICIONAR PARAMETRO PALYERS
+			System.out.println("Usage: java AdditionServer <server ip> <players>");
+			System.exit(1);
+		}
+
+		try {
+			System.setProperty("java.rmi.server.hostname", args[0]);
+			LocateRegistry.createRegistry(52369);
+			System.out.println("java RMI registry created.");
+		} catch (RemoteException e) {
+			System.out.println("java RMI registry already exists.");
+		}
+
+		try {
+			String server = "rmi://" + args[0] + ":52369/Hello";
+			Naming.rebind(server, new JogoServerImpl());
+			System.out.println("JogoServer is ready.");
+		} catch (Exception e) {
+			System.out.println("JogoServer failed: " + e);
+		}
+    }
 
     @Override
     public int registra() throws RemoteException {
